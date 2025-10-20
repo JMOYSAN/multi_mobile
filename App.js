538b5c797057c53d17bcd87bc564b0c4bca5263a
JMOYSAN <2375142@cstjean.qc.ca>
2025-10-20 14:26:17 -0400

    Ajout homescreen pour rendre l'app fonctionnel (FIX)

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
-import { useEffect } from 'react';
-import * as SplashScreen from 'expo-splash-screen';
-import { NavigationContainer } from '@react-navigation/native';
-import { RootNavigator } from './src/navigation/RootNavigator';
-SplashScreen.preventAutoHideAsync();
+import { useEffect } from 'react'
+import * as SplashScreen from 'expo-splash-screen'
+import { NavigationContainer } from '@react-navigation/native'
+import RootNavigator from './src/navigation/RootNavigator'  // ← no braces
+
+SplashScreen.preventAutoHideAsync()
+
 export default function App() {
     useEffect(() => {
-        const timer = setTimeout(() => SplashScreen.hideAsync(), 600);
-        return () => clearTimeout(timer);
-    }, []);
+        const timer = setTimeout(() => SplashScreen.hideAsync(), 600)
+        return () => clearTimeout(timer)
+    }, [])
+
     return (
         <NavigationContainer>
             <RootNavigator />
         </NavigationContainer>
-    );
+    )
 }
